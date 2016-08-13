fb453adbb72fad42d02c723f8e9cdae6b2a9376f
Kevin Ma <kbma.kevin@gmail.com>
2016-08-13 17:24:13 -0400

    create an application that declares SalesTransaction objects and displays their values and their sum

---
+++ SalesTransactionDemo/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalesTransactionDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SalesTransaction s1 = new SalesTransaction("Bob", 423.12, 0.02);
+            SalesTransaction s2 = new SalesTransaction("Sally", 4323.12, 0.01);
+
+            Console.WriteLine(s1);
+            Console.WriteLine();
+            Console.WriteLine(s2);
+            Console.WriteLine();
+
+            Console.WriteLine("SUM: {0:C}", (s1 + s2));
+        }
+    }
+}
---
+++ SalesTransactionDemo/SalesTransaction.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalesTransactionDemo
+{
+    class SalesTransaction
+    {
+        private string _salespersonName;
+        private double _salesAmt;
+        private readonly double _commision;
+
+        public SalesTransaction(string name, double salesAmt, double commRate)
+        {
+            this._salespersonName = name;
+            this._salesAmt = salesAmt;
+            this._commision = salesAmt * commRate;
+        }
+
+        public SalesTransaction(string name, double salesAmt) : this(name, salesAmt, 0)
+        {
+        }
+
+        public SalesTransaction(string name) : this(name, 0, 0)
+        {
+        }
+
+        public static double operator +(SalesTransaction s1, SalesTransaction s2)
+        {
+            return s1._salesAmt + s2._salesAmt;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("Salesperson Name: {0}\nSales Amount: {1:C}\nCommision: {2:C}",
+                this._salespersonName, this._salesAmt, this._commision);
+        }
+    }
+}
