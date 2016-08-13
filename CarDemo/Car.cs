20fe8b092a69453de967b508b2b6717a803f8c86
Kevin Ma <kbma.kevin@gmail.com>
2016-08-13 17:59:03 -0400

    application that demonstrates operator overloading

---
+++ CarDemo/Car.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarDemo
+{
+    class Car
+    {
+        public string Model { get; set; }
+        public int MilesPerGallon { get; set; }
+
+        public Car(string model, int mpg)
+        {
+            this.Model = model;
+            this.MilesPerGallon = mpg;
+        }
+
+        public Car(string model) : this(model, 20) { }
+
+        public static Car operator ++(Car c)
+        {
+            c.MilesPerGallon++;
+            return c;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("Model: {0}\nMiles per Gallon: {1}",
+                this.Model, this.MilesPerGallon);
+        }
+    }
+}
---
+++ CarDemo/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car c1 = new Car("Honda", 92);
+            Car c2 = new Car("BMW");
+
+            Console.WriteLine(c1);
+            Console.WriteLine();
+            Console.WriteLine(c2);
+            Console.WriteLine();
+
+            Console.WriteLine("Operator ++ testing +++++++++++++++++++++++++++++++++++++");
+            Console.WriteLine();
+            c1++;
+            Console.WriteLine(c1);
+        }
+    }
+}
