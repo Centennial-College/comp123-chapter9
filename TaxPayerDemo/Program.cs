9338b99c83ab4a66a6a49a3c8b5aed35daabd459
Kevin Ma <kbma.kevin@gmail.com>
2016-08-13 18:41:37 -0400

    Q6A

---
+++ TaxPayerDemo/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaxPayerDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Taxpayer[] taxpayers = new Taxpayer[10];
+
+            string ssn;
+            double income;
+
+            for (int i = 0; i < taxpayers.Length; i++)
+            {
+                Console.Write("Please enter your Social Security Number: ");
+                ssn = Console.ReadLine();
+
+                Console.Write("Please enter your yearly gross income: ");
+                income = int.Parse(Console.ReadLine());
+
+                taxpayers[i] = new Taxpayer(ssn, income);
+            }
+
+            foreach (Taxpayer t in taxpayers)
+            {
+                Console.WriteLine(t + "\n");
+            }
+
+        }
+    }
+}
---
+++ TaxPayerDemo/Taxpayer.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaxPayerDemo
+{
+    class Taxpayer
+    {
+        private double _yearlyGrossIncome;
+        private double _incomeTaxOwed;
+
+        public string SocialSecurityNumber { get; set; }
+        public double YearlyGrossIncome
+        {
+            get
+            {
+                return this._yearlyGrossIncome;
+            }
+            set
+            {
+                this._yearlyGrossIncome = value;
+                if (this.YearlyGrossIncome < 30000)
+                {
+                    this._incomeTaxOwed = this.YearlyGrossIncome * .15;
+                }
+                else
+                {
+                    this._incomeTaxOwed = this.YearlyGrossIncome * .28;
+                }
+            }
+        }
+        public double IncomeTaxOwed
+        {
+            get
+            {
+                return this._incomeTaxOwed;
+            }
+        }
+
+        public Taxpayer(string ssn, double income)
+        {
+            this.SocialSecurityNumber = ssn;
+            this.YearlyGrossIncome = income;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("Social Security Number: {0}\nIncome: {1:C}\nTax Owing: {2:C}",
+                this.SocialSecurityNumber, this.YearlyGrossIncome, this.IncomeTaxOwed);
+        }
+
+    }
+}
