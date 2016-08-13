bdd3730fb0f1288b14d9c9d6caaa74019b69733c
Kevin Ma <kbma.kevin@gmail.com>
2016-08-13 17:07:44 -0400

    created an application that instantiates and displays a ClassifiedAd object

---
+++ TestClassifiedAd/ClassifiedAd.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestClassifiedAd
+{
+    class ClassifiedAd
+    {
+        private string _category;
+        private int _numberOfWords;
+        private double _price;
+
+        public string Category
+        {
+            get
+            {
+                return this._category;
+            }
+            set
+            {
+                this._category = value;
+            }
+        }
+
+        public int NumberOfWords
+        {
+            get
+            {
+                return this._numberOfWords;
+            }
+            set
+            {
+                this._numberOfWords = value;
+                this._price = 0.09 * this.NumberOfWords;
+            }
+        }
+
+        public double Price
+        {
+            get
+            {
+                return this._price;
+            }
+        }
+
+        public ClassifiedAd(string category, int numWords)
+        {
+            this.Category = category;
+            this.NumberOfWords = numWords;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("Category: {0}\nNumber of Words: {1}\nPrice: {2:C}",
+                this.Category, this.NumberOfWords, this.Price);
+        }
+    }
+}
---
+++ TestClassifiedAd/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestClassifiedAd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ClassifiedAd c = new ClassifiedAd("Used Cars", 529);
+
+            Console.WriteLine(c);
+        }
+    }
+}
