7b06299e25b7b1e4519b859534764586ac21aeea
Kevin Ma <kbma.kevin@gmail.com>
2016-08-14 14:28:15 -0400

    added DemoJobs

---
+++ DemoJobs/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoJobs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Job j1 = new Job("wash windows", 3.5, 25);
+            Job j2 = new Job("peer tutor", 10, 12);
+            Job j3 = new Job("college faculty", 15, 50);
+
+            Console.WriteLine(j1);
+            Console.WriteLine();
+            Console.WriteLine(j2);
+            Console.WriteLine();
+            Console.WriteLine(j3);
+            Console.WriteLine();
+
+            Console.WriteLine("OPERATOR + OVERLOADED TESTING ++++++++++++++++++++++++++++++++");
+            Console.WriteLine(j1 + j2);
+        }
+    }
+}
