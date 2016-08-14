0b1e89a6ccc1aa87da14f88afe6ced9b67bec92a
Kevin Ma <kbma.kevin@gmail.com>
2016-08-14 08:41:45 -0400

    demo'd using a parameter array

---
+++ ShirtDemo/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShirtDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Shirt s1 = new Shirt("Cotton", "Blue", 'L');
+            Shirt s2 = new Shirt("Polyester", "Red", 'S');
+            Shirt s3 = new Shirt("Paper", "White", 'M');
+
+            Console.WriteLine("NO SHIRTS +++++++++++++++++++++++++++++++++++++++++++++");
+            Display();
+            Console.WriteLine();
+
+
+            Console.WriteLine("ONE SHIRT +++++++++++++++++++++++++++++++++++++++++++++");
+            Display(s1);
+            Console.WriteLine();
+
+            Console.WriteLine("TWO SHIRTS +++++++++++++++++++++++++++++++++++++++++++++");
+            Display(s1, s2);
+            Console.WriteLine();
+
+
+            Console.WriteLine("THREE SHIRTS +++++++++++++++++++++++++++++++++++++++++++++");
+            Display(s1, s2, s3);
+            Console.WriteLine();
+
+        }
+
+        public static void Display(params Shirt[] shirts)
+        {
+            foreach (Shirt aShirt in shirts)
+            {
+                Console.WriteLine(aShirt);
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ ShirtDemo/Shirt.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShirtDemo
+{
+    class Shirt
+    {
+        public string Material { get; set; }
+        public string Colour { get; set; }
+        public char Size { get; set; }
+
+        public Shirt(string mat, string color, char size)
+        {
+            this.Material = mat;
+            this.Colour = color;
+            this.Size = size;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("Material: {0}\nColour: {1}\nSize: {2}",
+                this.Material, this.Colour, this.Size);
+        }
+
+    }
+}
